81a164f215a656a9e75d129924f70e50f06b608c
nurulasad030 <nurulasad030@gmail.com>
2022-09-29 15:45:09 +0600

    test 4

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({cart}) => {
+    console.log(cart);
+
+    let total = 0;
+    let shipping =0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = (total * 0.1).toFixed(2) ;
+  
     return (
-        <div>
-             <h3>
-                    order summury in cart
-                </h3>
+        <div className='cart'>
+                <h3>
+                        order summury in cart
+                    </h3>
                 <p>selected item :{cart.length}</p>
+                <p>Total price : {total} </p>
+                <p>Total Shipping : {shipping}</p>
+                <p>Tax : {tax}</p>
         </div>
     );
 };
